e62e7700ac47a2413603a551c8c40ce46fbaf960
Kuldeep Gangwar <gangwarkuldeep123@gmail.com>
2024-07-05 14:38:47 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
+import withPWA from 'next-pwa';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  ...pwaConfig
+});
